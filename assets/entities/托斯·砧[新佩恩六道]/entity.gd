2a2183c3cad090de5e642134169cd44828f0be57
lele <2922218035@qq.com>
2026-08-11 13:12:00 +0800

    优化佐助攻击框，新增托斯角色

---
+++ assets/entities/托斯·砧[新佩恩六道]/entity.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2DEntity
+class_name 托斯·砧
+
+func _ready():
+	res_path = "res://assets/entities/托斯/main.tres"
+	super._ready()
+	#effects_container.register_effects({
+		#"YOUR_EFFECT_NAME": preload("YOUR_EFFECT_PATH"),
+	#})
