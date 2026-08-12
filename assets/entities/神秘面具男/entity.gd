703a876ad7a584be96b7abd26ee6280916d0be5d
lele <2922218035@qq.com>
2026-08-12 18:22:32 +0800

    添加神秘面具男，绑定骨架4a特效

---
+++ assets/entities/神秘面具男/entity.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2DEntity
+class_name 神秘面具男
+
+func _ready():
+	res_path = "res://assets/entities/神秘面具男/main.tres"
+	super._ready()
+	#effects_container.register_effects({
+		#"YOUR_EFFECT_NAME": preload("YOUR_EFFECT_PATH"),
+	#})
---
+++ assets/entities/神秘面具男/火球/entity.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2DEntity
+class_name 火球
+
+func _ready():
+	res_path = "res://assets/entities/神秘面具男/火球/main.tres"
+	super._ready()
+	#effects_container.register_effects({
+		#"YOUR_EFFECT_NAME": preload("YOUR_EFFECT_PATH"),
+	#})
